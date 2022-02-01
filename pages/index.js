6f0fb7b3f8971eb080bdd8054582d446e8ea2e8b
SEIFU MEKONNEN TOLOSA <tms.0xvoid@gmail.com>
2022-02-01 18:36:57 +0000

    setting up main pages

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,3 @@
-import Head from 'next/head'
+function HomePage() {}
 
-export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>Blog</title>
-      </Head>
-      <main>
-        <h1>The blog Home page</h1>
-      </main>
-      <footer></footer>
-    </div>
-  )
-}
+export default HomePage
