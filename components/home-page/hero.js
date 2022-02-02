e111c271b6686424ed1c3fb26bf8efca9b29a800
SEIFU MEKONNEN TOLOSA <tms.0xvoid@gmail.com>
2022-02-02 11:07:42 +0000

    Addin the hero component

---
+++ components/home-page/hero.js
@@ -0,0 +1,25 @@
+import Image from 'next/image'
+
+import classes from './hero.module.css'
+
+function Hero(props) {
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src="/images/site/admin.jpg"
+          alt="An Image showing the website adminstrator"
+          width={300}
+          height={300}
+        />
+      </div>
+      <h1>Hi I am Me</h1>
+      <p>
+        I blog about Blockchain - especially the Ethereum ecosystem, explore new
+        ideas and smart contract development
+      </p>
+    </section>
+  )
+}
+
+export default Hero
